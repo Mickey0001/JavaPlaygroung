76a50c3d5ca5a05a6cf0ecf057ba08a8080f7c6c
Mickey0001 <mirzasisic57@gmail.com>
2019-06-23 23:28:50 +0200

    Working on Browser Class

--- WebDriverTest/TestFramework/src/main/java/Browser.java
+++ WebDriverTest/TestFramework/src/main/java/Browser.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Browser {
-     System.setProperty("webdriver.chrome.driver", "C:/dev/tools/chromedriver.exe");
      static WebDriver driver = new ChromeDriver();
 
     public  static  void goTo(String url) {
